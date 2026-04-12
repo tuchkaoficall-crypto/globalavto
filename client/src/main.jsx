6e4da7aa20152054085204a3092c7c9d9fa750eb
GlobalAvto <globalavto@mail.ru>
2026-04-12 04:13:47 +0400

    metrika via main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,10 @@ import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './index.css'
 
+// Yandex Metrika
+;(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
+window.ym(108503034,'init',{webvisor:true,clickmap:true,accurateTrackBounce:true,trackLinks:true})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
